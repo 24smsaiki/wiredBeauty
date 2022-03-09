39d87c931cd7a4bd15d4d3e983469d3004f71190
Zaid ALAA HAZIM <zaid.anaee@yahoo.com>
2022-03-09 20:51:08 +0100

    Réajustements responsive + ajouts js transition images

--- assets/studies.js
+++ assets/studies.js
@@ -1,1 +1,20 @@
-import './styles/studies-services.css';
+let btnWithoutWB = document.getElementById('btnWithoutWB');
+let imgSteps1 = document.getElementById('imgSteps1');
+let btnWithWB = document.getElementById('btnWithWB');
+let imgSteps2 = document.getElementById('imgSteps2');
+
+imgSteps2.style.display = 'none';
+
+btnWithoutWB.onclick = function () {
+    document.getElementById('parentBtnWithoutWB').className = "cta-primary-1";
+    document.getElementById('parentBtnWithWB').className = "cta-primary-disabled";
+    imgSteps1.style.display = 'inline';
+    imgSteps2.style.display = 'none';
+}
+
+btnWithWB.onclick = function () {
+    document.getElementById('parentBtnWithoutWB').className = "cta-primary-disabled";
+    document.getElementById('parentBtnWithWB').className = "cta-primary-1";
+    imgSteps1.style.display = 'none';
+    imgSteps2.style.display = 'inline';
+}
